5bf968f63df769ab88945da0929cd480b7b3c74a
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2023-05-26 10:53:43 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,8 @@
 repeat task.wait(0.5) until game:IsLoaded() and game:IsLoaded() == true
 
 local GameList = {
-	["https://raw.githubusercontent.com/amogusimposter123123/Dark-Storm/scripts/War-Tycoon.lua"] = {4639625707}
+	["https://raw.githubusercontent.com/amogusimposter123123/Dark-Storm/scripts/War-Tycoon.lua"] = {4639625707},
+	["https://raw.githubusercontent.com/amogusimposter123123/Dark-Storm/scripts/Tower-Battles.lua"] = {49707852, 45146873, 46955756}
 }
 
 for link, idstable in pairs(GameList) do
