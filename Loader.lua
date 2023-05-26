f7ce9466740094dbfe0c6f1e02fb5842ddbcbe4a
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2023-05-26 10:56:35 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,8 @@
 repeat task.wait(0.5) until game:IsLoaded() and game:IsLoaded() == true
 
+if not game:IsLoaded() then
+    game["Loaded"]:Wait()
+end
 local GameList = {
 	["https://raw.githubusercontent.com/amogusimposter123123/Dark-Storm/scripts/War-Tycoon.lua"] = {4639625707},
 	["https://raw.githubusercontent.com/amogusimposter123123/Dark-Storm/scripts/Tower-Battles.lua"] = {49707852, 45146873, 46955756}
